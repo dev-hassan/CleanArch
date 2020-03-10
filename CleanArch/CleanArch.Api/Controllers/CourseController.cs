e58379e1bea1ee3a73a36a5c0237d9cc93df8e3e
mahmoud hassan <dev_m_hassan@hotmail.com>
2020-03-10 12:08:46 +0200

    Add MediatR to Presentation layer and Conig it , add Swagger with it's configuration

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+
+            return Ok(courseViewModel);
+        }
+    }
+}
--- CleanArch/CleanArch.Api/Startup.cs
+++ CleanArch/CleanArch.Api/Startup.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.IOC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CleanArch.MVC.Data;
-using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace CleanArch.MVC
+namespace CleanArch.Api
 {
     public class Startup
     {
@@ -30,20 +30,16 @@ namespace CleanArch.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("UniversityDBIdentityConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
             services.AddDbContext<UniversityDBContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("UniversityDBConnection")));
+            services.AddControllers();
 
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1",new OpenApiInfo()));
 
             services.AddMediatR(typeof(Startup));
+            
             RegisterServices(services);
         }
 
@@ -53,31 +49,22 @@ namespace CleanArch.MVC
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseSwagger(options =>  options.SerializeAsV2 = true);
+            app.UseSwaggerUI(c=>
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json","University Api V1"));
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
-
         private static void RegisterServices(IServiceCollection services)
         {
             DependencyContainer.RegisterServices(services);
